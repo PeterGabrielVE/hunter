fe4c34deadc31ebf4283b53ad94e2e0e6c5d2783
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-09-28 23:14:40 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,99 +6,141 @@
 
 @section('content')
 @include('layouts.header')
-<div class="row" style="m">
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+<div class="row">
+  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+        <li class="breadcrumb-item"><a href="#">Captura de Datos</a></li>
+      </ol>
+    </nav>
+  </div>
+  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
     <div class="card card-statistics">
       <div class="card-body">
         <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
           <div class="float-left">
-            <i class="mdi mdi-cube text-danger icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">Total Revenue</p>
+            <i class="mdi mdi-laptop text-info icon-lg"></i>
             <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">$65,650</h3>
+              <p class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+              Captura de Datos</p>
+              <h3 class="font-weight-medium text-left mb-0">Escaneo de Pasaportes</h3>
             </div>
           </div>
         </div>
-        <p class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
-          <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth </p>
       </div>
     </div>
   </div>
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-    <div class="card card-statistics">
-      <div class="card-body">
-        <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
-          <div class="float-left">
-            <i class="mdi mdi-receipt text-warning icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">Orders</p>
-            <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">3455</h3>
+  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 grid-margin stretch-card">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-calendar icon-lg"></i>
             </div>
           </div>
+          <h4 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Calendario </h4>
         </div>
-        <p class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
-          <i class="mdi mdi-bookmark-outline mr-1" aria-hidden="true"></i> Product-wise sales </p>
       </div>
     </div>
-  </div>
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-    <div class="card card-statistics">
-      <div class="card-body">
-        <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
-          <div class="float-left">
-            <i class="mdi mdi-poll-box text-success icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">Sales</p>
-            <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">5693</h3>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-history icon-lg"></i>
             </div>
           </div>
+          <h4 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Historial </h4>
         </div>
-        <p class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
-          <i class="mdi mdi-calendar mr-1" aria-hidden="true"></i> Weekly Sales </p>
       </div>
     </div>
-  </div>
-  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
-    <div class="card card-statistics">
-      <div class="card-body">
-        <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
-          <div class="float-left">
-            <i class="mdi mdi-account-box-multiple text-info icon-lg"></i>
-          </div>
-          <div class="float-right">
-            <p class="mb-0 text-right">Employees</p>
-            <div class="fluid-container">
-              <h3 class="font-weight-medium text-right mb-0">246</h3>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-help-circle icon-lg"></i>
             </div>
           </div>
+          <h4 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Ayuda </h4>
         </div>
-        <p class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
-          <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Product-wise sales </p>
       </div>
     </div>
   </div>
+  
+  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
+    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-hotel icon-lg"></i>
+            </div>
+          </div>
+          <h3 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Hoteles </h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-note-text icon-lg"></i>
+            </div>
+          </div>
+          <h3 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Registros </h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-database icon-lg"></i>
+            </div>
+          </div>
+          <h3 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Alertas </h3>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="d-flex flex-md-column flex-xl-row flex-wrap justify-content-between align-items-md-center justify-content-xl-between">
+            <div class="float-left">
+              <i class="mdi mdi-cube-outline icon-lg"></i>
+            </div>
+          </div>
+          <h3 class="text-muted mt-3 mb-0 text-left text-md-center text-xl-left">
+          Eventos </h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </div>
 <div class="row">
   <div class="col-md-12 grid-margin">
     <div class="card">
       <div class="card-body">
         <div class="d-sm-flex justify-content-between align-items-center mb-4">
-          <h2 class="card-title mb-0">Product Analysis</h2>
+          <h2 class="card-title mb-0">Visitantes Totales</h2>
           <div class="wrapper d-flex">
             <div class="d-flex align-items-center mr-3">
               <span class="dot-indicator bg-success"></span>
-              <p class="mb-0 ml-2 text-muted">Product</p>
+              <p class="mb-0 ml-2 text-muted">Visitantes</p>
             </div>
-            <div class="d-flex align-items-center">
-              <span class="dot-indicator bg-primary"></span>
-              <p class="mb-0 ml-2 text-muted">Resources</p>
-            </div>
+           
           </div>
         </div>
         <div class="chart-container">
